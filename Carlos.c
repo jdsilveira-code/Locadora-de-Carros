20ae629439fcd034cc1bc330829b24e9f2765588
Carlinbf <carlosbifesf@gmail.com>
2025-10-16 10:47:37 -0300

    .

--- Carlos.c
+++ Carlos.c
@@ -1,42 +1,64 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+}Data;
+typedef struct {
+    int Código[9];
+    char *Nome[9];
+    char buffer[50];
+}Categoria;
+typedef struct {
+    char Código[9];
+    char *Marca[100];
+    char *Modelo[100];
+    struct Categoria categoria;
+    int Ano[4];
+    float Diaria[10];
+    int qtdUnidades[101];
+    int qtdAlugadas[101];
+}Veículo;
+typedef struct{
+    int numero;
+    codigoVeiculo[9];
+    Data dataRetirada;
+    Data dataPrevistaDevolucao;
+    Data Devolucao;
+    int status;
+    float diariaAtrasoContrato;
+    float valorPrevisto;
+    float valorPago;
+}Locacao;
+void cadastrarCategoria(){
+}
+void exibirCategorias(){
+}
+void InserirVeiculo(){
+}
+void exibirFrota(){
+}
+void realizarAluguel(){
+}
+void realizarDevolucao(){
+}
+void alugadosCategoria(){
+}
+void alugadosGeral(){
+}
+void LocacoesAbertas(){
+}
+int main(){
     char frota[100];
     char categorias[11];
     char contratos[1001];
-    struct Data{
-        int dia;
-        int mes;
-        int ano;
-    };
-    struct Categoria{
-        int Código[9];
-        char *Nome[9];
-        char buffer[50];
-    };
-    struct Veículo{
-        char Código[9];
-        char *Marca[100];
-        char *Modelo[100];
-        struct Categoria categoria;
-        int Ano[4];
-        float Diaria[10];
-        int qtdUnidades[101];
-        int qtdAlugadas[101];
-    };
-    struct Locacao{
-        int numero;
-        codigoVeiculo[9];
-        dataRetirada(Data);
-        dataPrevistaDevolucao(Data);
-        dataDevolucao(Data);
-        int status;
-        float diariaAtrasoContrato;
-        float valorPrevisto;
-        float valorPago;
-    }
     switch (Digito){
     case 1:
-}
-void cadastrarCategoria(){
+    case 2:
+    case 3:
+    case 4:
+    }
 }
 
